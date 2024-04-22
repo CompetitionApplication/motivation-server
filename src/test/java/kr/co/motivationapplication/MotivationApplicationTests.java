5a0c207f0f8b89fa8ef96ae2fc54b8f1e2e57ae3
newy12 <newy12@naver.com>
2024-04-22 14:35:51 +0900

    #feat. first commit

---
+++ src/test/java/kr/co/motivationapplication/MotivationApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.motivationapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotivationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
