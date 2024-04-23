a4ed4c06bd42e6387cc815c9c9c078e86613edc4
newy12 <newy12@naver.com>
2024-04-23 10:37:20 +0900

    #feat. jpa mybatis 혼용 사용을 위한 기본세팅 및 예시코드 추가

---
+++ module-database/src/main/java/kr/co/repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package kr.co.repository;
+
+import kr.co.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member,Long> {
+}
