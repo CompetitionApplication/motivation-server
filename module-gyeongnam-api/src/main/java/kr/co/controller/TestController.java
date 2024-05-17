5d1efce3c93a40c5304abe4860f647bcb126540e
newy12 <newy12@naver.com>
2024-05-17 12:58:36 +0900

    #feat. jenkins test4p

--- module-gyeongnam-api/src/main/java/kr/co/controller/TestController.java
+++ module-gyeongnam-api/src/main/java/kr/co/controller/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
-@RequiredArgsConstructorπ
+@RequiredArgsConstructor
 public class TestController {
-//test5
+//test6
 }
