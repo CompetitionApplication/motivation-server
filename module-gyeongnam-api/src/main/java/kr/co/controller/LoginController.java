67d1a708ed471d4d58e2fdf852cbb4bf17cad753
newy12 <newy12@naver.com>
2024-06-03 11:59:01 +0900

    #fix. 배포테스틑

--- module-gyeongnam-api/src/main/java/kr/co/controller/LoginController.java
+++ module-gyeongnam-api/src/main/java/kr/co/controller/LoginController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
 
     private final LoginService loginService;
-    //test
+    //testtest
 /*
     @PostMapping("")
     public ResponseEntity<?> login(@RequestBody @Valid LoginReqDto loginReqDto) throws Exception {
