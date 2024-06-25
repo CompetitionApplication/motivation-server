25ef5e5855b7ce99e4489475e6bb7e7db8f5face
namgihyeon <skarlgus0419@naver.com>
2024-06-25 12:53:33 +0900

    #feat. 메일 비동기 추가

--- module-agriculture-api/src/main/java/kr/co/common/mail/MailService.java
+++ module-agriculture-api/src/main/java/kr/co/common/mail/MailService.java
@@ -1,8 +1,10 @@
 package kr.co.common.mail;
 
 import kr.co.dto.common.mail.ReservationMailDto;
+import org.springframework.scheduling.annotation.Async;
 
 public interface MailService {
 
+    @Async
     void sendReservationMail(ReservationMailDto reservationMailDto) throws Exception;
 }
--- module-agriculture-api/src/main/java/kr/co/common/mail/MailServiceImpl.java
+++ module-agriculture-api/src/main/java/kr/co/common/mail/MailServiceImpl.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
@@ -20,6 +21,7 @@ public class MailServiceImpl implements MailService{
     final SpringTemplateEngine templateEngine;
     final MailMapper mailMapper;
 
+    @Async
     @Override
     public void sendReservationMail(ReservationMailDto reservationMailDto) throws Exception{
 
