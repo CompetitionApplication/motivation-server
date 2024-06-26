e7e454d0b247ae72fec09253b1ec0c3ec34ef73b
newy12 <newy12@naver.com>
2024-06-26 10:20:17 +0900

    #feat. 관광 세팅1

--- module-tourism-api/src/main/java/kr/co/ModuleTourismApiApplication.java
+++ module-tourism-api/src/main/java/kr/co/ModuleTourismApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ModuleTourismApiApplication {
-    //vnkv
+    //vnkv33
 
     public static void main(String[] args) {
         SpringApplication.run(ModuleTourismApiApplication.class, args);
