3c8946d9970c6b6bc4ecedac4c381afc326ec08c
youngjae <newy12@naver.com>
2024-08-18 22:23:35 +0900

    #feat. 굿즈 등록/목록/수정/삭제 스웨거 설명 추가

--- module-tourism-api/src/main/java/kr/co/controller/admin/GoodsController.java
+++ module-tourism-api/src/main/java/kr/co/controller/admin/GoodsController.java
@@ -1,6 +1,7 @@
 package kr.co.controller.admin;
 
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.co.dto.GoodsUploadReqDto;
 import kr.co.service.GoodsService;
@@ -17,24 +18,28 @@ import org.springframework.web.bind.annotation.*;
 public class GoodsController {
     private final GoodsService goodsService;
 
+    @Operation(summary = "굿즈 목록 리스트", description = "굿즈 목록 리스트 입니다.")
     @GetMapping("/list")
     public ResponseEntity<?> getGoodsList(@RequestParam(defaultValue = "0") int page,
                                           @RequestParam(defaultValue = "10") int size) {
         return ResponseEntity.ok(goodsService.getGoodsList(page, size));
     }
 
+    @Operation(summary = "굿즈 업로드", description = "굿즈 업로드를 합니다.")
     @PostMapping("/upload")
     public ResponseEntity<?> uploadGoods(@ModelAttribute GoodsUploadReqDto goodsUploadReqDto) {
         goodsService.uploadGoods(goodsUploadReqDto);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "굿즈 삭제", description = "굿즈 삭제를 합니다.")
     @DeleteMapping("/delete/{goodsId}")
     public ResponseEntity<?> deleteGoods(@PathVariable(value = "goodsId") String goodsId) {
         goodsService.deleteGoods(goodsId);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(summary = "굿즈 수정", description = "굿즈 수정을 합니다.")
     @PutMapping("/update/{goodsId}")
     public ResponseEntity<?> updateGoods(@PathVariable(value = "goodsId") String goodsId,
                                          @ModelAttribute GoodsUploadReqDto goodsUploadReqDto) {
