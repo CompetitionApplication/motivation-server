675efe9690fb3a0f6fe3a3f0647379e5eb8e2fd4
newy12 <newy12@naver.com>
2024-09-03 13:03:15 +0900

    #feat. 경남쪽 마이바티스 설정잘못들어가서 삭제함

--- module-database/module-database-gyeongnam/src/main/java/kr/co/mapper/common/CommonMapper.java
+++
@@ -1,9 +0,0 @@
-package kr.co.mapper.common;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface CommonMapper {
-    String selectUUID();
-    String reservationId();
-}
--- module-database/module-database-gyeongnam/src/main/java/kr/co/mapper/common/MailMapper.java
+++
@@ -1,9 +0,0 @@
-package kr.co.mapper.common;
-
-import kr.co.dto.common.mail.ReservationMailDto;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface MailMapper {
-    void insertMailSend(ReservationMailDto reservationMailDto);
-}
