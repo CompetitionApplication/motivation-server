3c2b5c89849570bdae6aa576153a608f7a148e4e
youngjae <newy12@naver.com>
2024-09-20 00:16:50 +0900

    #feat. 장바구니 테이블추가

---
+++ module-database/module-database-tourism/src/main/java/kr/co/entity/UserCart.java
@@ -0,0 +1,37 @@
+package kr.co.entity;
+
+import jakarta.persistence.*;
+import kr.co.config.BooleanConverter;
+import kr.co.entity.common.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
+import org.hibernate.annotations.GenericGenerator;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserCart extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name="uuid2", strategy = "uuid2")
+    @Column(columnDefinition = "varchar(38)")
+    @Comment(value = "방문 키값")
+    private String userCartId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "goods_id")
+    private Goods goods;
+
+    @Column(columnDefinition = "varchar(1) default 'N'")
+    @Convert(converter = BooleanConverter.class)
+    private boolean delYn;
+
+}
