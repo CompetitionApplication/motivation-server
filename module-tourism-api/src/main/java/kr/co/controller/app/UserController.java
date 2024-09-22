591b24d38ffdf22675ca2064d935e4bdea5c444b
namgihyeon <skarlgus0419@naver.com>
2024-09-22 10:54:44 +0900

    [feat] 유저컨트롤러 -> 엔드 포인트 수정

--- module-tourism-api/src/main/java/kr/co/controller/app/UserController.java
+++ module-tourism-api/src/main/java/kr/co/controller/app/UserController.java
@@ -1,6 +1,7 @@
 package kr.co.controller.app;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import kr.co.common.ObjectResult;
 import kr.co.dto.LoginReqDto;
@@ -12,9 +13,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "login", description = "로그인/회원가입")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/user")
+@RequestMapping("/api/v1/app/user")
 public class UserController {
 
     private final UserService userService;
