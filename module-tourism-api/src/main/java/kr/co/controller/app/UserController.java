87470d4bef7c1a88aefdf3f38a3e2143866c21da
youngjae <newy12@naver.com>
2024-09-22 14:20:35 +0900

    #feat. test

--- module-tourism-api/src/main/java/kr/co/controller/app/UserController.java
+++ module-tourism-api/src/main/java/kr/co/controller/app/UserController.java
@@ -40,7 +40,7 @@ public class UserController {
     public ResponseEntity<?> myPage(@AuthenticationPrincipal ServiceUser serviceUser) {
         return ObjectResult.build(userService.myPage(serviceUser));
     }
-
+//tst
     @Operation(summary = "굿즈 구매 배송내역", description = "굿즈 구매 배송내역을 보여줍니다.")
     @GetMapping("/mypage/goods")
     public ResponseEntity<?> goods(@AuthenticationPrincipal ServiceUser serviceUser) {
