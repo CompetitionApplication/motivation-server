b1451f876916e773fe89a108d2c049ecdb74266f
namgihyeon <skarlgus0419@naver.com>
2024-09-25 23:28:49 +0900

    #fix. qr시간 수정

--- module-tourism-api/src/main/java/kr/co/service/app/QrService.java
+++ module-tourism-api/src/main/java/kr/co/service/app/QrService.java
@@ -13,6 +13,7 @@ import kr.co.repository.TourismVisitRepository;
 import kr.co.repository.UserBadgeRepository;
 import kr.co.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,6 +22,7 @@ import java.time.format.DateTimeFormatter;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class QrService {
 
     private final UserBadgeRepository userBadgeRepository;
@@ -32,8 +34,6 @@ public class QrService {
     public void receiveQrData(QrReqDto qrReqDto, ServiceUser serviceUser) {
         //qr 유효기간 체크
         String timeStr = qrReqDto.getQrTime();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
-        LocalDateTime dateTime = LocalDateTime.parse(timeStr, formatter);
 
         // 현재 시간 구하기
         LocalDateTime now = LocalDateTime.now();
@@ -41,7 +41,22 @@ public class QrService {
         // 30초 전 시간 구하기
         LocalDateTime nowMinus30Seconds = now.minusSeconds(30);
 
-        if (dateTime.isBefore(nowMinus30Seconds)) {
+        // 포맷터 설정 (yyyyMMddHHmmss 형식)
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+
+        // 현재 시간 포맷팅
+        String currentTimeStr = now.format(formatter);
+
+        // 30초 전 시간 포맷팅
+        String timeMinus30SecondsStr = nowMinus30Seconds.format(formatter);
+
+        log.info("요청 시간 ::: {}",timeStr);
+        log.info("30초전 시간 ::: {}",timeMinus30SecondsStr);
+        log.info("현재 시간 ::: {}",currentTimeStr);
+
+        if ( !(Integer.parseInt(timeStr) <= Integer.parseInt(currentTimeStr) &&
+               Integer.parseInt(timeStr) >= Integer.parseInt(timeMinus30SecondsStr))
+        ) {
             throw new CommonException(CommonErrorCode.ERROR_QR.getCode(), CommonErrorCode.ERROR_QR.getMessage());
         }
 
